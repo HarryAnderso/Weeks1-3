49ab163e588bbd636a4a88ae80e3ea8ee927d03d
Harry Anderson <andeharr@shernet.sheridancollege.ca>
2025-01-16 11:02:17 -0500

    hd

---
+++ Assets/scripts/AnimationCurvecode.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationCurvecode : MonoBehaviour
+{
+    public AnimationCurve curve;
+
+    [Range(0,1)]
+    public float t;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        transform.localScale = Vector2.one * curve.Evaluate(t);
+    }
+}
---
+++ Assets/scripts/lerpfunction.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class lerpfunction : MonoBehaviour
+{
+    public Vector2 start;
+        public Vector2 end;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //transform.position = Vector2.Lerp( t);
+    }
+}
